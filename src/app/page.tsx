92444c7e8370ae7a6b47d3476d810f09e463e6f6
MohamadSafi <the.mohammad.safi@gmail.com>
2023-10-07 01:26:51 +0300

    Adding the newsletter logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import ParticleBackground from "@/components/ParticleBackground";
 import { TimerContainer } from "@/components/TimerContainer";
 import Image from "next/image";
 import { EmailInput } from "@/components/EmailInput";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
 
 export default function Home() {
   const [email, setEmail] = useState<string>("");
@@ -59,29 +61,33 @@ export default function Home() {
     }
 
     // Use your chosen library to write the email to a Google Sheet
-    // try {
-    //   // Import and initialize the library here
-    //   const { GoogleSpreadsheet } = require("google-spreadsheet");
-    //   const doc = new GoogleSpreadsheet("YOUR_SPREADSHEET_ID");
+    try {
+      const serviceAccountAuth = new JWT({
+        email: "bott-138@motaz-users.iam.gserviceaccount.com",
+        key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC555W8BcfLJXxz\n5ysKdnxj1ZWh/PZZcceot+wQAW8V6Ux1qur0J2AUb6ZSMsNeRpndI5xsfQf+lvEV\nvfl3CzgO//ecCpJ61bD/Z8N9mgpiVcnsSexQpZlVYMjBaUc+9CrjRx75NYHRnPp5\n8jsPFuPxBbizP2typfZgaMPU6lE0/bXkVE8caLO6c21Oi6fUcPx7BzHNZA/BofWm\nrtqierL+4AoASHv1mmhUVCMW1Xgc8tPegCADnb1rOoXnZrnFCqkecjz3anwRJOT4\n+rWJkf9hUkfr9AFCKKqBbodDJS6RcZTFzCe1lUMLdvvzG7wuEat3vurFLHluev+X\nqVST4UddAgMBAAECggEAG20980MkDoW9hmj2ueCwqdbvMDqeiqQlaXAXCOZ5QdGN\nSPfaGYK8o080icB4/83pMc2o7H2Gwin5L/S62vQCBX6AhJovsaUYq5mepwdknEzd\nUTm3vk8sH8M7YwN58iD6OdU6jdg1ypu+/WI+EdQV9W9baECFROEUx/T03VLjf65l\nOAcEauMmwlVHEpR2AEvObsEFrppllEqH1D9oSdMtWqmTGkDAQN2LjnBwOvWSbH0w\nPVPtmmhSf6fINV9TJWe39pLvzU1rllSmjeJrzn1B3EctxvHmtrjbiL0nSyXsxtGK\nRLTALfjo6IbMiEwaSBfFK4OCNjgEufyIURYumXEP2QKBgQDd2iOMoQN9aJuTp3Eq\nH5/R7nIEs5AN000HCDJnI1xynKlLDc2hfqUzfwOKGUN8Yeq4NKe+hUi6KhiSjw16\nMs111zIkdO2wSZJXyK09IUYhPtmMJ6g4G1mQ4fuRYv+4/6uYHQqRBvccwBvC3toX\nD6nFOW5BgGvtAYgtrJ+crGAZKQKBgQDWhPi3Qh33FhcREP2ujmka9z8pGXtbh+3J\nnd1BxesxP+h5AzsCqsCV1scf0Hp7PhH8DKc1SavTq0xUqE1EWCHTltEKgO7E9+S2\nffDLrGnbYU6nQcyV9imBfWGhX2i7mfWreVrrlNwfTlzUc1asHDNE8A0BoTQzpdxW\n7daDcYFfFQKBgDBSqnmgPMVdOr3/s9uMBeDRQk6Z2h/YzadhdrEWd7WTQCDUAd1S\nW6OK3/d9p0B5h4nWFk+ecofyUWXi+PmMBUrMs+RS/87tR44NbQaqr77gFLy9ipum\nAW1Ye9ZgLPlb30GD2eBbFwpRIfrhVbm+NKl75/MmiNuYANSKDD4xIcF5AoGAY9Zs\n/rU5xvYz9BBpUGUdC5zIe3NNkxLXGz9qayMWy8wHgNJ9E0eu682SzP61EkiYDCtD\nYqyP2KpfkwLlOwqda6FEVtQcy/TnL/u7Jr9uYNiPx437DkwLkyVsW7NQxgbobE7F\nfuC/9Z393C51xxI01j+2feyto9/shVSZwoEACeUCgYAqfkee6y9NOytG2WVgfv1L\n0JEyMGCXehajqr/8oVvUxMOGPt96c3T+Yy0eObzoK4GTwairYH13xSOLGhfko3SV\nT8rY+pvaYgJh+g7WYgmAWCr0TQ98duiirSEGePvd9X12VV83WVfRjl+S33zP7102\n7tGnY9laYGoSmp8fYllQpw==\n-----END PRIVATE KEY-----\n",
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+      });
+      console.log("auth done");
 
-    //   // Authenticate with Google Sheets (You need to set up credentials)
-    //   await doc.useServiceAccountAuth({
-    //     client_email: "YOUR_CLIENT_EMAIL",
-    //     private_key: "YOUR_PRIVATE_KEY",
-    //   });
+      const doc = new GoogleSpreadsheet(
+        "1J-p4RTXUL7i2vZFtF9Zacq1PoyWrWqRqji5EjQgEB6c",
+        serviceAccountAuth
+      );
 
-    //   // Load the sheet
-    //   await doc.loadInfo();
-    //   const sheet = doc.sheetsByIndex[0]; // Assuming you want to write to the first sheet
+      // Authenticate with Google Sheets (You need to set up credentials)
 
-    //   // Add a new row with the email address
-    //   await sheet.addRow({ Email: email });
+      // Load the sheet
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0]; // Assuming you want to write to the first sheet
 
-    //   alert("Thank you for subscribing!");
-    // } catch (error) {
-    //   console.error("Error writing to Google Sheet:", error);
-    //   alert("An error occurred while subscribing. Please try again later.");
-    // }
+      // Add a new row with the email address
+      await sheet.addRow({ Email: email });
+
+      alert("Thank you for subscribing!");
+    } catch (error) {
+      console.error("Error writing to Google Sheet:", error);
+      alert("An error occurred while subscribing. Please try again later.");
+    }
   };
 
   const handleChange = (e: any) => {
